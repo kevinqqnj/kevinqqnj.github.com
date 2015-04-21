97294f55a08298c53ab1e5be06013c183bf6e70d
kevinqqnj <ezhqing@gmail.com>
2015-04-21 10:08:01 +0800

    add CGI test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+﻿#!/usr/local/bin/python
+import cgi 
+def main():
+    print "Content-type: text/html\n"
+    form = cgi.FieldStorage() # parse query
+    if form.has_key("firstname") and form["firstname"].value != "":
+        print "<h1>Hello", form["firstname"].value, "</h1>"
+    else:
+        print "<h1>Error! Please enter first name.</h1>" 
+main()
